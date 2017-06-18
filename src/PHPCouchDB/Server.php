026d5475cbf74177719b5aa131d89b14ed27d148
Lorna Jane Mitchell <lorna@lornajane.net>
2017-06-18 14:50:15 +0100

    Fix coding standards on existing code

--- src/PHPCouchDB/Server.php
+++ src/PHPCouchDB/Server.php
@@ -2,23 +2,26 @@
 
 namespace PHPCouchDB;
 
-class Server {
+class Server
+{
     protected $client;
 
-    public function __construct(\GuzzleHttp\ClientInterface $client) {
-        if($client) {
+    public function __construct(\GuzzleHttp\ClientInterface $client)
+    {
+        if ($client) {
             $this->client = $client;
         } else {
             throw new Exception('$client is required');
         }
     }
 
-    public function getVersion() {
+    public function getVersion()
+    {
         $response = $this->client->request("GET", "/");
-        if($response->getStatusCode() == 200) {
+        if ($response->getStatusCode() == 200) {
             // try to decode JSON
-            if($json_data = json_decode($response->getBody(), true)) {
-                if($json_data['version']) {
+            if ($json_data = json_decode($response->getBody(), true)) {
+                if ($json_data['version']) {
                     return $json_data['version'];
                 } else {
                     return "unknown";
