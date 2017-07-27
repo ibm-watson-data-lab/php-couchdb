5cff08417ec1c74ed3333e12cc4a05567c67fc29
Lorna Jane Mitchell <lorna@lornajane.net>
2017-07-27 12:21:50 +0100

    Allow URLs as well as Guzzle clients in the constructor

--- src/PHPCouchDB/Server.php
+++ src/PHPCouchDB/Server.php
@@ -16,12 +16,32 @@ class Server
 {
     protected $client;
 
-    public function __construct(\GuzzleHttp\ClientInterface $client)
+    /**
+     * Create the object to represent the server
+     *
+     * See also: \PHPCouchDB\Server:createFromURL()
+     *
+     * @param array $options Supply either a string "url" parameter OR a
+     *  \GuzzleHttp\ClientInterface "client" parameter if more configuration
+     *  is required
+     * @throws \PHPCouchDB\Exception\ServerException if there's a problem with parsing arguments or creating the client
+     */
+    public function __construct(array $options)
     {
-        if ($client) {
-            $this->client = $client;
+        if(empty($options) || !is_array($options)) {
+            throw new \PHPCouchDB\Exception\ServerException('$options is a required parameter, array should contain either a url or a client');
+        }
+
+        if(isset($options['client']) && $options['client'] instanceof \GuzzleHttp\ClientInterface) {
+            $this->client = $options['client'];
+        } elseif(isset($options['url'])) {
+            try {
+                $this->client = new \GuzzleHttp\Client(["base_uri" => $options['url']]);
+            } catch(Exception $e) {
+                throw new \PHPCouchDB\Exception\ServerException("Could not connect with URL.  Error: " . $e->getMessage());
+            }
         } else {
-            throw new Exception('$client is required');
+            throw new \PHPCouchDB\Exception\ServerException('Failed to parse $options, array should contain either a url or a client');
         }
     }
 
--- tests/PHPCouchDB/ServerTest.php
+++ tests/PHPCouchDB/ServerTest.php
@@ -11,6 +11,25 @@ use GuzzleHttp\Exception\RequestException;
 
 class ServerTest extends \PHPUnit\Framework\TestCase 
 {
+    public function testCreateWithURL() {
+        $url = "http://localhost:5984";
+
+        $server = new \PHPCouchDB\Server(["url" => $url]);
+
+        $this->assertObjectHasAttribute('client', $server);
+        $this->assertAttributeInstanceOf('\GuzzleHttp\ClientInterface', 'client', $server);
+    }
+
+    public function testCreateWithClient() {
+        $url = "http://localhost:5984";
+        $client = new \GuzzleHttp\Client(["base_uri" => $url]);
+
+        $server = new \PHPCouchDB\Server(["client" => $client]);
+
+        $this->assertObjectHasAttribute('client', $server);
+        $this->assertAttributeInstanceOf('\GuzzleHttp\ClientInterface', 'client', $server);
+    }
+
     public function testGetVersion() {
 
 		$couchdb1 = '{"couchdb":"Welcome","uuid":"fce3d5aabfe189c988273c0ffa8d375b","version":"1.6.0","vendor":{"name":"Ubuntu","version":"15.10"}}';
@@ -22,9 +41,8 @@ class ServerTest extends \PHPUnit\Framework\TestCase
 		$handler = HandlerStack::create($mock);
 		$client = new Client(['handler' => $handler]);
 
-
 		// userland code starts
-		$server = new \PHPCouchDB\Server($client);
+		$server = new \PHPCouchDB\Server(["client" => $client]);
 		$this->assertEquals("1.6.0", $server->getVersion());
 
     }
