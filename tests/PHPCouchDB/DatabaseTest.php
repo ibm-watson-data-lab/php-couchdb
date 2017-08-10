18641a83c0ed7f16adf9a1767de0da082257768b
Lorna Jane Mitchell <lorna@lornajane.net>
2017-08-10 10:23:21 +0100

    Enable adding records to the database

--- tests/PHPCouchDB/DatabaseTest.php
+++ tests/PHPCouchDB/DatabaseTest.php
@@ -62,4 +62,47 @@ class DatabaseTest extends \PHPUnit\Framework\TestCase
         $this->assertInternalType('array', $docs);
         $this->assertEmpty($docs);
     }
+
+    public function testCreateWithID() {
+        $egdb1 = '{"db_name":"egdb","update_seq":"0-g1AAAABXeJzLYWBgYMpgTmEQTM4vTc5ISXLIyU9OzMnILy7JAUklMiTV____PyuRAY-iPBYgydAApP5D1GYBAJmvHGw","sizes":{"file":8488,"external":0,"active":0},"purge_seq":0,"other":{"data_size":0},"doc_del_count":0,"doc_count":0,"disk_size":8488,"disk_format_version":6,"data_size":0,"compact_running":false,"instance_start_time":"0"}';
+        $use_response = new Response(200, [], $egdb1);
+        $create = '{"ok":true,"id":"abcde12345","rev":"1-928ec193918889e122e7ad45cfd88e47"}';
+        $create_response = new Response(201, [], $create);
+        $fetch = '{"_id":"abcde12345","_rev":"1-928ec193918889e122e7ad45cfd88e47","noise":"howl"}';
+        $fetch_response = new Response(200, [], $fetch);
+
+		$mock = new MockHandler([ $this->db_response, $use_response, $create_response, $fetch_response ]);
+		$handler = HandlerStack::create($mock);
+		$client = new Client(['handler' => $handler]);
+
+		// userland code starts
+		$server = new \PHPCouchDB\Server(["client" => $client]);
+        $database = $server->useDB(["name" => "egdb"]);
+        $doc = $database->create(["noise" => "howl", "id" => "abcde12345"]);
+
+        $this->assertInstanceOf('PHPCouchDB\Document', $doc);
+        $this->assertObjectHasAttribute('id', $doc);
+        $this->assertEquals("abcde12345", $doc->id);
+    }
+
+    public function testCreateWithoutID() {
+        $egdb1 = '{"db_name":"egdb","update_seq":"0-g1AAAABXeJzLYWBgYMpgTmEQTM4vTc5ISXLIyU9OzMnILy7JAUklMiTV____PyuRAY-iPBYgydAApP5D1GYBAJmvHGw","sizes":{"file":8488,"external":0,"active":0},"purge_seq":0,"other":{"data_size":0},"doc_del_count":0,"doc_count":0,"disk_size":8488,"disk_format_version":6,"data_size":0,"compact_running":false,"instance_start_time":"0"}';
+        $use_response = new Response(200, [], $egdb1);
+        $create = '{"ok":true,"id":"95613816b3a7490727388ebb47002c0f","rev":"1-928ec193918889e122e7ad45cfd88e47"}';
+        $create_response = new Response(201, [], $create);
+        $fetch = '{"_id":"95613816b3a7490727388ebb47002c0f","_rev":"1-928ec193918889e122e7ad45cfd88e47","noise":"howl"}';
+        $fetch_response = new Response(200, [], $fetch);
+
+		$mock = new MockHandler([ $this->db_response, $use_response, $create_response, $fetch_response ]);
+		$handler = HandlerStack::create($mock);
+		$client = new Client(['handler' => $handler]);
+
+		// userland code starts
+		$server = new \PHPCouchDB\Server(["client" => $client]);
+        $database = $server->useDB(["name" => "egdb"]);
+        $doc = $database->create(["noise" => "howl"]);
+
+        $this->assertInstanceOf('PHPCouchDB\Document', $doc);
+        $this->assertObjectHasAttribute('id', $doc);
+    }
 }
